4dd2f111be4532dbefd7be9f990a057f2f071aa8
Pat Thoyts <patthoyts@users.sourceforge.net>
2011-12-01 15:17:39 +0000

    Simplify building with msvc9 and 10.
    More recent Microsoft compilers include the direct show headers but miss
    qedit.h. This patch allows the project to be compiled using Visual
    Studio 2008 and 2010 without specifically requiring the DirectX SDK to
    be available. MSVC 6 still needs the platform sdk and directx sdk headers.
    
    Signed-off-by: Pat Thoyts <patthoyts@users.sourceforge.net>

--- win/graph.h
+++ win/graph.h
@@ -4,7 +4,19 @@
 #define WIN32_LEAN_AND_MEAN
 #define OEMRESOURCE 
 #include <windows.h>
+
+// The following section allows us to compile using Visual Studio 2008 and
+// beyond without requiring the installation of DX9.
+#if _MSC_VER < 1500
 #include <qedit.h>
+#else
+#pragma include_alias("dxtrans.h","qedit.h")
+#define __IDxtCompositor_INTERFACE_DEFINED__
+#define __IDxtAlphaSetter_INTERFACE_DEFINED__
+#define __IDxtJpeg_INTERFACE_DEFINED__
+#define __IDxtKey_INTERFACE_DEFINED__
+#include "dx/qedit.h"
+#endif
 
 #ifdef HAVE_WMF_SDK
 #include <dshowasf.h>
